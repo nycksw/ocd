a07a4a89469f8c97ed02fffdd31f04452b1334d3
Ken Wehr <ksw@ksw-glaptop.corp.google.com>
2010-07-31 20:30:59 -0400

    Ditching Gnome. Modifying Openbox configs. Adding Amnesiac settings. Misc other shit.

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,23 +1,13 @@
 # ~eater/.config/openbox/autostart.sh
 # http://eater.org/
 
-xsetroot -solid black
-
 . $GLOBALAUTOSTART
-
-ln -sf ~/.bgimg-$(hostname -f) ~/.bgimg
-ln -sf ~/.synergyrc-$(hostname -f) ~/.synergyrc
 
 xset fp+ ~/.fonts
 xset fp rehash
 xset b off
 xset m 2 1.6
 
-if [ -f ~/.synergy-$(hostname -f) ];then
-  sh ~/.synergy-$(hostname -f) &
-fi
-
 xscreensaver -nosplash &
 
 which pypanel && (sleep 2 && pypanel) &
-which fbsetbg && (sleep 5 && fbsetbg ~/.bgimg) &
